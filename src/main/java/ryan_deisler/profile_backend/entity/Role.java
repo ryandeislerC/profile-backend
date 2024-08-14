8ea748bbffdc21c862a7ce213bf89af15cc24fb1
Ryan <ryan.deisler@capgemini.com>
2024-08-14 17:03:28 -0400

    Add role and employee entities

---
+++ src/main/java/ryan_deisler/profile_backend/entity/Role.java
@@ -0,0 +1,32 @@
+package ryan_deisler.profile_backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    private String[] summary;
+    @Column(name = "start_date")
+    private String startDate;
+    @Column(name = "end_date")
+    private String endDate;
+    private String company;
+}
